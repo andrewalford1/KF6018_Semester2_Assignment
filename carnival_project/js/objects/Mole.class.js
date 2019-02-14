03c528d6135f59b12fbabd7a8b173c879653de0a
Sabina-Irimia <36729732+Sabina-Irimia@users.noreply.github.com>
2019-02-14 20:46:48 +0000

    Renamed the Moles.class.js to Mole.class.js
    Added the paws to the mole.

--- carnival_project/js/objects/Mole.class.js
+++ carnival_project/js/objects/Mole.class.js
@@ -81,28 +81,67 @@
             moleRightPawMesh.receiveShadow = true;
             moleRightPawMesh.position.set(-0.07, 0.07, 0.06);
 
-            //Create the fingers for the paws of the mole
-            let moleFingerGeometry = new THREE.CylinderGeometry(0.012, 0.006, 0.05, widthSegments, heightSegments, false, 0, 6.3);//3.0
-            let moleFingerMaterial = new THREE.MeshPhysicalMaterial({ color: LIGHT_SKIN_COLOR, metalness: 0.0, 
-                                                                      roughness: 0.6, reflectivity: 0.2, side: THREE.DoubleSide });
-            let moleFingerMesh = new THREE.Mesh(moleFingerGeometry, moleFingerMaterial);
-            moleFingerMesh.rotation.set(0.0, 0.0, 0.2);
-            moleFingerMesh.castShadow = true;
-            moleFingerMesh.receiveShadow = true;
-            moleFingerMesh.position.set(0.06, 0.045, 0.09);
+            // Create group for the left paw of the mole
+            let moleLeftPawGroup = new THREE.Group();
+            moleLeftPawGroup.add(moleLeftPawMesh);
 
-            // Create group for the paws of the mole
-            let molePawGroup = new THREE.Group();
-            molePawGroup.add(moleLeftPawMesh);
-            molePawGroup.add(moleRightPawMesh);
-            molePawGroup.add(moleFingerMesh);
+            // Create group for the right paw of the mole
+            let moleRightPawGroup = new THREE.Group();
+            moleRightPawGroup.add(moleRightPawMesh);
+
+//------------------------------------------------------------
+            //Create the fingers for the paws of the mole
+           let moleFingerGeometry = [];
+           let moleFingerMaterial = [];
+           let moleFingerMesh = [];
+           let moleFingerNumber = 10;
+           for (let i=0; i<moleFingerNumber; i++){
+                   moleFingerGeometry.push(new THREE.CylinderGeometry(0.012, 0.006, 0.05, 18, 18, false, 0, 6.3));//3.0
+                   moleFingerMaterial.push(new THREE.MeshPhysicalMaterial({ color: 0xF5C8AA, metalness: 0.5, 
+                                                             roughness: 0.6, reflectivity: 0.2, side: THREE.DoubleSide }));
+                   moleFingerMesh.push(new THREE.Mesh(moleFingerGeometry[i], moleFingerMaterial[i]));
+                   moleFingerMesh[i].castShadow = true;
+                   moleFingerMesh[i].receiveShadow = true;
+
+                   if(i <= 4){
+                       moleLeftPawGroup.add(moleFingerMesh[i]);    
+                   }
+                   else if( i=>5){
+                        moleRightPawGroup.add(moleFingerMesh[i]);  
+                   }
+           }
+          //Position each finger of the paws
+          //Fingers for the left paw
+          moleFingerMesh[0].position.set(0.06, 0.045, 0.091);
+          moleFingerMesh[0].rotation.set(-0.03, 0.0, 0.2);
+          moleFingerMesh[1].position.set(0.072, 0.045, 0.081);
+          moleFingerMesh[1].rotation.set(-0.03, 0.0, 0.1);
+          moleFingerMesh[2].position.set(0.082, 0.045, 0.07);
+          moleFingerMesh[2].rotation.set(-0.09, 0.0, 0.08);
+          moleFingerMesh[3].position.set(0.092, 0.045, 0.061);
+          moleFingerMesh[3].rotation.set(-0.03, 0.0, 0.0);
+          moleFingerMesh[4].position.set(0.101, 0.045, 0.050);
+          moleFingerMesh[4].rotation.set(-0.02, 0.0, -0.02);
+
+          //Fingers for the right paw
+          moleFingerMesh[5].position.set(-0.06, 0.045, 0.091);
+          moleFingerMesh[5].rotation.set(-0.03, 0.0, -0.2);
+          moleFingerMesh[6].position.set(-0.072, 0.045, 0.081);
+          moleFingerMesh[6].rotation.set(-0.03, 0.0, -0.1);
+          moleFingerMesh[7].position.set(-0.082, 0.045, 0.07);
+          moleFingerMesh[7].rotation.set(-0.09, 0.0, -0.08);
+          moleFingerMesh[8].position.set(-0.092, 0.045, 0.061);
+          moleFingerMesh[8].rotation.set(-0.03, 0.0, -0.0);
+          moleFingerMesh[9].position.set(-0.101, 0.045, 0.050);
+          moleFingerMesh[9].rotation.set(-0.03, 0.0, 0.02);
 
           //Create a group for the whole body of the mole 
           let moleBodyPartsGroup = new THREE.Group();
           moleBodyPartsGroup.add(moleHeadMesh);
           moleBodyPartsGroup.add(moleBodyMesh);
           moleBodyPartsGroup.add(moleNoseMesh);
-          moleBodyPartsGroup.add(molePawGroup);
+          moleBodyPartsGroup.add(moleLeftPawGroup);
+          moleBodyPartsGroup.add(moleRightPawGroup);
 
           //Add the sphere to the object group.
           this.addObjectToGroup(moleBodyPartsGroup);
