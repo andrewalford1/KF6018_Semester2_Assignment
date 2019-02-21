e16436735d898427e1401cfbbf9887c471aa179c
Zoe Irwin_ <35450972+zoeirwin@users.noreply.github.com>
2019-02-21 14:17:15 +0000

    Update main.js
    fixing lukes bit

--- carnival_project/js/main.js
+++ carnival_project/js/main.js
@@ -26,7 +26,6 @@ engineDriver.getObjectManager().addObject(
 engineDriver.getObjectManager().addObject(
     new BasicCharacter(new THREE.Vector3(25, 0, 0))
 );
-
 engineDriver.getObjectManager().addObject(
     new Goal(new THREE.Vector3(25, 0, 0))
 );
