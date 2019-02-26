d4368ad3ee7f214c87c4b10f7f082644fa3b29ad
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-02-26 16:43:14 +0000

    Update FerrisWheel.class.js
    Added animations

--- carnival_project/js/objects/FerrisWheel.class.js
+++ carnival_project/js/objects/FerrisWheel.class.js
@@ -21,8 +21,19 @@ class FerrisWheel extends ENGINE.OBJECTS.ClassicObject
             'ferris_wheel',
             'gltf'
         );
+        //Scale and position the model.
+        FERRIS_WHEEL.model.scale.set(7, 7, 7);
+        FERRIS_WHEEL.model.position.set(-250, -160, -500);
+        FERRIS_WHEEL.model.rotation.y = 315 * (Math.PI / 180);
 
+        //Add the model to the group.
         this.addObjectToGroup(FERRIS_WHEEL.model);
+
+        //[onStart] Checks if this is the very first frame of animation.
+        let onStart = true;
+
+        //[mixer] Will manage the different animations of the  model.
+        let mixer = new THREE.AnimationMixer(FERRIS_WHEEL.model);
 
         /**
          * Updates the tent. (Overridden from the superclass).
@@ -31,7 +42,28 @@ class FerrisWheel extends ENGINE.OBJECTS.ClassicObject
          */
         this.update = function(frameTime)
         {
-            //Circus Tent does not need to update.
+            //Initialise the object on the fist frame.
+            if(onStart)
+            {
+                //If the ferris wheel has animations, animate it.
+                if(FERRIS_WHEEL.animations)
+                {
+                    //Initialise the animation.
+                    mixer.clipAction(
+                        THREE.AnimationClip.findByName(
+                            FERRIS_WHEEL.animations,
+                            FERRIS_WHEEL.animations[0].name
+                        )).play();
+                }
+                //Prevent further access to this code.
+                onStart = false;
+            }
+
+            //Update the animation.
+            if(mixer)
+            {
+                mixer.update(frameTime / 2000);
+            }
         }
     }
 }
