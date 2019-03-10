19c1917f4b8455defaddb4ddb7d206d88663522f
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-03-10 22:30:21 +0000

    Can now add models via JSON

--- carnival_project/js/main.js
+++ carnival_project/js/main.js
@@ -13,45 +13,59 @@ let light = new THREE.HemisphereLight(0xFFFFFF, 0x444444);
 light.position.set(0, 20, 0);
 engineDriver.getScene().add(light);
 
-//ADD OBJECTS...
-engineDriver.getObjectManager().addObjects([
-    //Characters
-    new BasicCharacter(new THREE.Vector3(25, 0, 0)),
-    new Bear(new THREE.Vector3(0, 0, 0)),
-    new Boy(new THREE.Vector3(25, 0, 0)),
-    new GirlSitting(new THREE.Vector3(0, 0, 0)),
-    new Jack(new THREE.Vector3(0, 0, 0)),
-    //Darts Game
-    new Dart(new THREE.Vector3(0, 0, 0)),
-    new DartsGame(new THREE.Vector3(0, 0, 0)),
-    //Enviroment
-    new BalloonArch(new THREE.Vector3(0, 0, 0)),
-    new Carousel(new THREE.Vector3(0, 0, 0)),
-    new CircusTent(new THREE.Vector3(50, 0, 0)),
-    new FerrisWheel(new THREE.Vector3(0, 0, 0)),
-    new Helicopter(new THREE.Vector3(0, 0, 0)),
-    new HotAirBalloon(new THREE.Vector3(0, 250, 0)),
-    new MoreBalloons(new THREE.Vector3(0, 0, 0)),
-    new RollerCoaster(new THREE.Vector3(0, 0, 0)),
-    new SmallTent(new THREE.Vector3(0, 0, 0)),
-    new StreetLamp(new THREE.Vector3(0, 0, 0)),
-    new Teacup(new THREE.Vector3(25, 0, 0)),
-    new Terrain(new THREE.Vector3(0, 0, 0)),
-    new Welcome(new THREE.Vector3(0, 0, 0)),
-    //Football Game
-    new Football(new THREE.Vector3(0, 0, 0)),
-    new Goal(new THREE.Vector3(25, 0, 0)),
-    //Strength-o-Meter Game
-    new StrengthOMetre(new THREE.Vector3(0, 0, 0)),
-    //Whack-a-Mole Game
-    new WhackAMole(new THREE.Vector3(0, 0, 0))
-]);
-engineDriver.getObjectManager().addAllToScene(engineDriver.getScene());
-engineDriver.getObjectManager().setAllActive(true);
+
+//Load models from JSON file
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    
+    //ADD OBJECTS USING JSON...
+    ENGINE.DEBUGGER.extractJSON(this.responseText)
+    .enviroment.forEach(model => {
+        engineDriver.getObjectManager().addObject(
+            new ENGINE.OBJECTS.Model(model));
+        console.table(model);    
+    });
+
+    //ADD OBJECTS USING JS CLASSES...
+    engineDriver.getObjectManager().addObjects([
+        //Characters
+        new BasicCharacter(new THREE.Vector3(25, 0, 0)),
+        new Boy(new THREE.Vector3(25, 0, 0)),
+        new GirlSitting(new THREE.Vector3(0, 0, 0)),
+        new Jack(new THREE.Vector3(0, 0, 0)),
+        //Darts Game
+        new DartsGame(new THREE.Vector3(0, 0, 0)),
+        //Enviroment
+        new BalloonArch(new THREE.Vector3(0, 0, 0)),
+        new Carousel(new THREE.Vector3(0, 0, 0)),
+        new CircusTent(new THREE.Vector3(50, 0, 0)),
+        new FerrisWheel(new THREE.Vector3(0, 0, 0)),
+        new Helicopter(new THREE.Vector3(0, 0, 0)),
+        new HotAirBalloon(new THREE.Vector3(0, 250, 0)),
+        new MoreBalloons(new THREE.Vector3(0, 0, 0)),
+        new RollerCoaster(new THREE.Vector3(0, 0, 0)),
+        new SmallTent(new THREE.Vector3(0, 0, 0)),
+        new StreetLamp(new THREE.Vector3(0, 0, 0)),
+        new Teacup(new THREE.Vector3(25, 0, 0)),
+        new Terrain(new THREE.Vector3(0, 0, 0)),
+        //Football Game
+        new Football(new THREE.Vector3(0, 0, 0)),
+        new Goal(new THREE.Vector3(25, 0, 0)),
+        //Strength-o-Meter Game
+        new StrengthOMetre(new THREE.Vector3(0, 0, 0)),
+        //Whack-a-Mole Game
+        new WhackAMole(new THREE.Vector3(0, 0, 0))
+    ]);
+    engineDriver.getObjectManager().addAllToScene(engineDriver.getScene());
+    engineDriver.getObjectManager().setAllActive(true);
+  }
+};
+xmlhttp.open("GET", "json/models.json", true);
+xmlhttp.send();
 
 //Skybox.
 ENGINE.TextureLoader().loadSkybox('skybox', '.bmp', engineDriver.getScene());
-
 
 /**
  * Animates the project.
