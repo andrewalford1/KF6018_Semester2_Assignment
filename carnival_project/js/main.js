eec9fad072e02e1d602047c06d69f438bd8c69ac
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-03-11 15:39:56 +0000

    Local model loading (hopefully)

--- carnival_project/js/main.js
+++ carnival_project/js/main.js
@@ -5,7 +5,7 @@ let engineDriver = ENGINE.Driver(
     new THREE.Scene(),
     ENGINE.Camera(new THREE.Vector3(0, 25, 50), false),
     ENGINE.ObjectManager(),
-    true
+    false //Debug Mode
 );
 
 //ADD LIGHTING... (I have not figured out a good way to do lighting yet).
@@ -13,54 +13,92 @@ let light = new THREE.HemisphereLight(0xFFFFFF, 0x444444);
 light.position.set(0, 20, 0);
 engineDriver.getScene().add(light);
 
+ENGINE.DEBUGGER.extractJSON(MODELS)
+.enviroment.forEach(model => {
+    engineDriver.getObjectManager().addObject(
+        new ENGINE.OBJECTS.Model(model));
+});
 
-//Load models from JSON file
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
+//ADD OBJECTS USING JS CLASSES...
+engineDriver.getObjectManager().addObjects([
+    //Characters
+    new BasicCharacter(new THREE.Vector3(25, 0, 0)),
+    new Boy(new THREE.Vector3(25, 0, 0)),
+    new GirlSitting(new THREE.Vector3(0, 0, 0)),
+    new Jack(new THREE.Vector3(0, 0, 0)),
+    //Darts Game
+    new DartsGame(new THREE.Vector3(0, 0, 0)),
+    //Enviroment
+    new BalloonArch(new THREE.Vector3(0, 0, 0)),
+    new Carousel(new THREE.Vector3(0, 0, 0)),
+    new CircusTent(new THREE.Vector3(50, 0, 0)),
+    new Helicopter(new THREE.Vector3(0, 0, 0)),
+    new HotAirBalloon(new THREE.Vector3(0, 250, 0)),
+    new MoreBalloons(new THREE.Vector3(0, 0, 0)),
+    new RollerCoaster(new THREE.Vector3(0, 0, 0)),
+    new SmallTent(new THREE.Vector3(0, 0, 0)),
+    new StreetLamp(new THREE.Vector3(0, 0, 0)),
+    new Teacup(new THREE.Vector3(25, 0, 0)),
+    new Terrain(new THREE.Vector3(0, 0, 0)),
+    //Football Game
+    new Football(new THREE.Vector3(0, 0, 0)),
+    new Goal(new THREE.Vector3(25, 0, 0)),
+    //Strength-o-Meter Game
+    new StrengthOMetre(new THREE.Vector3(0, 0, 0)),
+    //Whack-a-Mole Game
+    new WhackAMole(new THREE.Vector3(0, 0, 0))
+]);
+engineDriver.getObjectManager().addAllToScene(engineDriver.getScene());
+engineDriver.getObjectManager().setAllActive(true);
+
+
+// //Load models from JSON file
+// let xmlhttp = new XMLHttpRequest();
+// xmlhttp.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
     
-    //ADD OBJECTS USING JSON...
-    ENGINE.DEBUGGER.extractJSON(this.responseText)
-    .enviroment.forEach(model => {
-        engineDriver.getObjectManager().addObject(
-            new ENGINE.OBJECTS.Model(model));
-    });
+//     //ADD OBJECTS USING JSON...
+//     ENGINE.DEBUGGER.extractJSON(this.responseText)
+//     .enviroment.forEach(model => {
+//         engineDriver.getObjectManager().addObject(
+//             new ENGINE.OBJECTS.Model(model));
+//     });
 
-    //ADD OBJECTS USING JS CLASSES...
-    engineDriver.getObjectManager().addObjects([
-        //Characters
-        new BasicCharacter(new THREE.Vector3(25, 0, 0)),
-        new Boy(new THREE.Vector3(25, 0, 0)),
-        new GirlSitting(new THREE.Vector3(0, 0, 0)),
-        new Jack(new THREE.Vector3(0, 0, 0)),
-        //Darts Game
-        new DartsGame(new THREE.Vector3(0, 0, 0)),
-        //Enviroment
-        new BalloonArch(new THREE.Vector3(0, 0, 0)),
-        new Carousel(new THREE.Vector3(0, 0, 0)),
-        new CircusTent(new THREE.Vector3(50, 0, 0)),
-        new Helicopter(new THREE.Vector3(0, 0, 0)),
-        new HotAirBalloon(new THREE.Vector3(0, 250, 0)),
-        new MoreBalloons(new THREE.Vector3(0, 0, 0)),
-        new RollerCoaster(new THREE.Vector3(0, 0, 0)),
-        new SmallTent(new THREE.Vector3(0, 0, 0)),
-        new StreetLamp(new THREE.Vector3(0, 0, 0)),
-        new Teacup(new THREE.Vector3(25, 0, 0)),
-        new Terrain(new THREE.Vector3(0, 0, 0)),
-        //Football Game
-        new Football(new THREE.Vector3(0, 0, 0)),
-        new Goal(new THREE.Vector3(25, 0, 0)),
-        //Strength-o-Meter Game
-        new StrengthOMetre(new THREE.Vector3(0, 0, 0)),
-        //Whack-a-Mole Game
-        new WhackAMole(new THREE.Vector3(0, 0, 0))
-    ]);
-    engineDriver.getObjectManager().addAllToScene(engineDriver.getScene());
-    engineDriver.getObjectManager().setAllActive(true);
-  }
-};
-xmlhttp.open("GET", "json/models.json", true);
-xmlhttp.send();
+//     //ADD OBJECTS USING JS CLASSES...
+//     engineDriver.getObjectManager().addObjects([
+//         //Characters
+//         new BasicCharacter(new THREE.Vector3(25, 0, 0)),
+//         new Boy(new THREE.Vector3(25, 0, 0)),
+//         new GirlSitting(new THREE.Vector3(0, 0, 0)),
+//         new Jack(new THREE.Vector3(0, 0, 0)),
+//         //Darts Game
+//         new DartsGame(new THREE.Vector3(0, 0, 0)),
+//         //Enviroment
+//         new BalloonArch(new THREE.Vector3(0, 0, 0)),
+//         new Carousel(new THREE.Vector3(0, 0, 0)),
+//         new CircusTent(new THREE.Vector3(50, 0, 0)),
+//         new Helicopter(new THREE.Vector3(0, 0, 0)),
+//         new HotAirBalloon(new THREE.Vector3(0, 250, 0)),
+//         new MoreBalloons(new THREE.Vector3(0, 0, 0)),
+//         new RollerCoaster(new THREE.Vector3(0, 0, 0)),
+//         new SmallTent(new THREE.Vector3(0, 0, 0)),
+//         new StreetLamp(new THREE.Vector3(0, 0, 0)),
+//         new Teacup(new THREE.Vector3(25, 0, 0)),
+//         new Terrain(new THREE.Vector3(0, 0, 0)),
+//         //Football Game
+//         new Football(new THREE.Vector3(0, 0, 0)),
+//         new Goal(new THREE.Vector3(25, 0, 0)),
+//         //Strength-o-Meter Game
+//         new StrengthOMetre(new THREE.Vector3(0, 0, 0)),
+//         //Whack-a-Mole Game
+//         new WhackAMole(new THREE.Vector3(0, 0, 0))
+//     ]);
+//     engineDriver.getObjectManager().addAllToScene(engineDriver.getScene());
+//     engineDriver.getObjectManager().setAllActive(true);
+//   }
+// };
+// xmlhttp.open("GET", "json/models.json", true);
+// xmlhttp.send();
 
 //Skybox.
 ENGINE.TextureLoader().loadSkybox('skybox', '.bmp', engineDriver.getScene());
