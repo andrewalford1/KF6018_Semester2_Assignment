91c68c9f32d19d7d6f9172555d577155d0b8365d
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-03-30 15:43:24 +0000

    fixed lighting bug in moon

--- carnival_project/js/objects/environment/Moon.class.js
+++ carnival_project/js/objects/environment/Moon.class.js
@@ -19,7 +19,7 @@ class Moon extends ENGINE.OBJECTS.ClassicObject
         //ADD LIGHTING... (I have not figured out a good way to do lighting yet).
         let moonLight = new THREE.HemisphereLight(0x375D9C, 0x444444, 1);//0xFFFFFF, 0x444444
         //moonLight.position.set(0.0, 20.0, 0.0);
-        moonLight.castShadow = true;
+        //moonLight.castShadow = true;
         this.addObjectToGroup(moonLight);
         /**
         //Add the HotAirBalloon model.
