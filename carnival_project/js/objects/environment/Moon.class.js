eb6e39ed587186f57821f2d0d6d9a2a790ea652d
Sabina-Irimia <sabinairimia@yahoo.com>
2019-03-30 13:41:29 +0000

    Update Moon.class.js

--- carnival_project/js/objects/environment/Moon.class.js
+++ carnival_project/js/objects/environment/Moon.class.js
@@ -15,24 +15,11 @@ class Moon extends ENGINE.OBJECTS.ClassicObject
     {
         //Construct the superclass.
         super(position);
-/**
-        let moonGeometry = new THREE.OctahedronGeometry(2,2);
-        let moonMaterial = new THREE.MeshPhysicalMaterial({
-                          emissive: 0xFFC900, emissiveIntensity: 1, color: 0x3E3200, flatShading: THREE.FlatShading,
-                          metalness: 0.0, roughness: 0.0, reflectivity: 0.0,  transparency: true, opacity: 0.5} );//ff0051
 
-        let moonLight = new THREE.PointLight( 0xffffff, 3, 100, 2);
-        moonLight.add( new THREE.Mesh( moonGeometry, moonMaterial ) );
-        moonLight.position.set(0.0, 0.0, 0.0);
-        moonLight.castShadow = true;
-        moonLight.shadow.mapSize.width = 524;
-        moonLight.shadow.mapSize.height = 524;
-        */
         //ADD LIGHTING... (I have not figured out a good way to do lighting yet).
         let moonLight = new THREE.HemisphereLight(0x375D9C, 0x444444, 1);//0xFFFFFF, 0x444444
         moonLight.position.set(0.0, 20.0, 0.0);
-        //moonLight.castShadow = true;
-        //engineDriver.getScene().add(moonLight);
+        moonLight.castShadow = true;
 
 
         this.addObjectToGroup(moonLight);
