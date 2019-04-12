63e4d16c7edaece98280e282fbdb0a1a49dc4fdc
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-04-12 18:50:09 +0100

    moved player position information UG class

--- carnival_project/js/utility/kinectFactory.js
+++ carnival_project/js/utility/kinectFactory.js
@@ -14,38 +14,6 @@ let kinectFactory = (function() {
                 if(player.geustures) {
                     player.geustures.update();
                 }
-
-                if(player.handsTogether()) {
-                    console.log('hands together');
-                };
-
-                if(player.leftHandAboveHead()) {
-                    console.log('left hand Above head');
-                }
-
-                if(player.rightHandAboveHead()) {
-                    console.log('right hand above head');
-                }
-
-                if(player.leftHandTouchingHead()) {
-                    console.log('left hand touching head');
-                }
-
-                if(player.rightHandTouchingHead()) {
-                    console.log('right hand touching head');
-                }
-
-                if(player.armsSpread()) {
-                    console.log('T-Pose');
-                }
-
-                if(player.leftHandAboveShoulder()) {
-                    console.log('Left hand above shoulder');
-                }
-
-                if(player.rightHandAboveShoulder()) {
-                    console.log('Right hand above shoulder');
-                }
             });
         }
     };
