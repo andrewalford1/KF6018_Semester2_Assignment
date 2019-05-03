c54a6d01a1416fbf3bfa0f6fb5c5aaf8f3d10467
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-05-03 11:47:31 +0100

    Removed some models

--- carnival_project/json/models.js
+++ carnival_project/json/models.js
@@ -24,52 +24,10 @@ let MODELS = `{
         "animated"      :   true,
         "animationSpeed":   2000
     }, { 
-        "fileName"      :   "boy_obj", 
-        "extension"     :   "glb", 
-        "position"      :   [ 35, 0, 0 ],
-        "scale"         :   0.05
-    }, { 
         "fileName"      :   "balloonArch", 
         "extension"     :   "glb", 
         "position"      :   [ 45, 0, -100 ],
         "scale"         :   0.3,
-        "rotation"      :   [ 90, 0, 0 ]
-    }, { 
-        "fileName"      :   "carousel", 
-        "extension"     :   "glb", 
-        "position"      :   [ 220, 0, -40 ],
-        "scale"         :   0.01,
-        "animated"      :   true,
-        "animationSpeed":   2000
-    }, { 
-        "fileName"      :   "circus_tent", 
-        "extension"     :   "glb", 
-        "position"      :   [ -250, 0, -500 ],
-        "scale"         :   0.1,
-        "rotation"      :   [ 90, 0, 0 ]
-    }, { 
-        "fileName"      :   "basic_character", 
-        "extension"     :   "glb", 
-        "position"      :   [ 25, 0, 0 ],
-        "scale"         :   10        
-    }, { 
-        "fileName"      :   "teacup_obj", 
-        "extension"     :   "glb", 
-        "position"      :   [ -125, 0, -70 ],
-        "scale"         :   0.1
-    }, {
-        "fileName"      :   "girlSitting", 
-        "extension"     :   "glb", 
-        "position"      :   [ 160, 0, 30 ],
-        "scale"         :   0.2,
-        "rotation"      :   [ 0, 90, 0 ],
-        "animated"      :   true,
-        "animationSpeed":   2000
-    }, { 
-        "fileName"      :   "jack", 
-        "extension"     :   "glb", 
-        "position"      :   [ -100, 0, 0 ],
-        "scale"         :   2,
         "rotation"      :   [ 90, 0, 0 ]
     }, { 
         "fileName"      :   "moreBalloons", 
